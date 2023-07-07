b463f1250d01aa14ceef0fd3dac6462159b50039
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-07-07 10:35:00 +0700

    Test Insert multiple users to farm 070723 1034

--- app/utils/genarateAccessToken.js
+++ app/utils/genarateAccessToken.js
@@ -2,7 +2,10 @@ const jwt = require('jsonwebtoken');
 
 class GenerateAccessToken {
     static GenerateAccessTokenForOwner(userAccount_ID, userDetail_ID, roleName) {
-        return jwt.sign({ userAccount_ID: userAccount_ID, userDetail_ID: userDetail_ID, role: roleName })
+        return jwt.sign(
+            { userAccount_ID: userAccount_ID, userDetail_ID: userDetail_ID, role: roleName },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '120m' });
     }
 }
 
