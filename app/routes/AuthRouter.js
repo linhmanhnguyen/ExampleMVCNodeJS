4ce84dfb9815bcc802be66d9ae86585a20227d48
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-07-07 19:20:14 +0700

    Update route check exist username 070723 1920

--- app/routes/AuthRouter.js
+++ app/routes/AuthRouter.js
@@ -4,5 +4,5 @@ const router = require('express').Router();
 
 router.post('/login', AuthController.Login);
 router.post('/register', AuthController.Register);
-router.get('/:username', AuthController.CheckExistUsername);
+router.get('/checkExistUsername/:username', AuthController.CheckExistUsername);
 module.exports = router;
