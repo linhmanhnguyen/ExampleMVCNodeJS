14b9716ef91f8ad4e79ff2c559897772ce9efbaa
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-07-18 15:44:29 +0700

    update route post animal to cage 180723 1544

--- app/validations/animalSchema.js
+++ app/validations/animalSchema.js
@@ -5,7 +5,7 @@ const animalSchema = Joi.object({
     gender_Animal: Joi.string().valid('male', 'female').required(),
     weight: Joi.number().positive().required(),
     entry_Date: Joi.date().required(),
-    status: Joi.boolean().required(),
+    status: Joi.string().valid('normal', 'sick', 'dead').required(),
 });
 
 const updateAnimalSchema = Joi.object({
