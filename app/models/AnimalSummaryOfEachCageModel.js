54df5f5e3a8df8eea686a79c31c604f75c1375a7
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-22 16:11:53 +0700

    update code 220823 1611

--- app/models/AnimalSummaryOfEachCageModel.js
+++ app/models/AnimalSummaryOfEachCageModel.js
@@ -1,9 +1,9 @@
 class AnimalSummaryOfEachCageModel {
-    constructor(cage_id, manager_fullname, totalAnimals, healtyAnimals, sickAnimals, deadAnimals) {
+    constructor(cage_id, manager_fullname, totalAnimals, healthyAnimals, sickAnimals, deadAnimals) {
         this.cage_id = cage_id;
         this.manager_fullname = manager_fullname;
         this.totalAnimals = totalAnimals;
-        this.healtyAnimals = healtyAnimals;
+        this.healthyAnimals = healthyAnimals;
         this.sickAnimals = sickAnimals;
         this.deadAnimals = deadAnimals;
     }
