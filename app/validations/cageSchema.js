abe6863e70d5debf63063141db1703864df7173a
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-28 14:02:17 +0700

    update route 'insert cage' 280823 1402

--- app/validations/cageSchema.js
+++ app/validations/cageSchema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const insertCageSchema = Joi.object({
     livestockStaff_id: Joi.number().integer().required(),
     veterinaryStaff_id: Joi.number().integer().required(),
-    dateEntryCage: Joi.date().format('DD-MM-YYYY'),
+    dateEntryCage: Joi.date().format('DD-MM-YYYY').raw(),
     numberOfAnimalsInCage: Joi.number().integer(),
     totalWeight: Joi.number(),
 });
