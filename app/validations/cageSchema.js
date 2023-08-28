47953619b3bb336c7111be054c45b42eaca8aec7
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-28 15:46:08 +0700

    update route 'insert cage' 280823 1546

--- app/validations/cageSchema.js
+++ app/validations/cageSchema.js
@@ -3,24 +3,12 @@ const JoiDate = require('@joi/date');
 
 const ExtendedJoi = Joi.extend(JoiDate); // Mở rộng Joi với joi-date
 
-const insertCageSchema = ExtendedJoi.object({
-    livestockStaff_id: ExtendedJoi.number().integer().required(),
-    veterinaryStaff_id: ExtendedJoi.number().integer().required(),
-    dateEntryCage: ExtendedJoi.when(
-        'dateEntryCage', { // Kiểm tra nếu dateEntryCage không rỗng
-        is: ExtendedJoi.exist(),
-        then: ExtendedJoi.date().format('DD-MM-YYYY').raw() // Bắt buộc có định dạng hợp lệ
-    }),
-    numberOfAnimalsInCage: ExtendedJoi.when(
-        'numberOfAnimalsInCage', { // Kiểm tra nếu numberOfAnimalsInCage không rỗng
-        is: ExtendedJoi.exist(),
-        then: ExtendedJoi.number().integer() // Bắt buộc phải có
-    }),
-    totalWeight: ExtendedJoi.when(
-        'totalWeight', { // Kiểm tra nếu totalWeight không rỗng
-        is: ExtendedJoi.exist(),
-        then: ExtendedJoi.number().integer() // Bắt buộc phải có
-    })
+const insertCageSchema = Joi.object({
+    livestockStaff_id: Joi.number().integer().required(),
+    veterinaryStaff_id: Joi.number().integer().required(),
+    // dateEntryCage: ExtendedJoi.date().format('DD-MM-YYYY'),
+    // numberOfAnimalsInCage: Joi.number().integer(),
+    // totalWeight: Joi.number(),
 });
 
 module.exports = {
