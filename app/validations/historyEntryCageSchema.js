9f6aae2a6a50ef63d2c76f7112f45c19c6d0afa9
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-28 09:26:16 +0700

    Test route 'insert cage' 280823 0926

--- app/validations/historyEntryCageSchema.js
+++ app/validations/historyEntryCageSchema.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 
 const insertEntryCage = Joi.object({
-    typeAnimal_id: Joi.number().integer().positive().required(),
     animalQuantity: Joi.number().integer().positive().required(),
     weightOfAnimal: Joi.number().positive().required(),
     unitPrice: Joi.number().positive().required(),
