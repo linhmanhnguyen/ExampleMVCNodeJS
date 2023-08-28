643e31274b27541d40c516ecf96b8be800192dbd
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-28 14:53:25 +0700

    update route register' 280823 1453

--- app/utils/genarateAccessToken.js
+++ app/utils/genarateAccessToken.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 class GenerateAccessToken {
-    static GenerateAccessTokenForOwner(userAccount_ID, userDetail_ID, roleName) {
+    static GenerateAccessTokenForOwner(userAccount_ID, userDetail_ID, roleName, farm_ID) {
         return jwt.sign(
-            { userAccount_ID: userAccount_ID, userDetail_ID: userDetail_ID, role: roleName },
+            { userAccount_ID: userAccount_ID, userDetail_ID: userDetail_ID, role: roleName, farm_ID: farm_ID },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '120m' });
     }
