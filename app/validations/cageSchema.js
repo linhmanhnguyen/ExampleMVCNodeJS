4df1fc8a4838e34c1a54466d9695709d3e166ac0
Phạm Đức Chiến <phamchienvn91@gmail.com>
2023-08-28 15:23:49 +0700

    update route 'insert cage' 280823 1523

--- app/validations/cageSchema.js
+++ app/validations/cageSchema.js
@@ -6,20 +6,20 @@ const ExtendedJoi = Joi.extend(JoiDate); // Mở rộng Joi với joi-date
 const insertCageSchema = ExtendedJoi.object({
     livestockStaff_id: ExtendedJoi.number().integer().required(),
     veterinaryStaff_id: ExtendedJoi.number().integer().required(),
-    dateEntryCage: ExtendedJoi.date().format('DD-MM-YYYY').raw().when(
+    dateEntryCage: ExtendedJoi.when(
         'dateEntryCage', { // Kiểm tra nếu dateEntryCage không rỗng
         is: ExtendedJoi.exist(),
-        then: ExtendedJoi.required() // Bắt buộc có định dạng hợp lệ
+        then: ExtendedJoi.date().format('DD-MM-YYYY').raw() // Bắt buộc có định dạng hợp lệ
     }),
-    numberOfAnimalsInCage: ExtendedJoi.number().integer().when(
+    numberOfAnimalsInCage: ExtendedJoi.when(
         'numberOfAnimalsInCage', { // Kiểm tra nếu numberOfAnimalsInCage không rỗng
         is: ExtendedJoi.exist(),
-        then: ExtendedJoi.required() // Bắt buộc phải có
+        then: ExtendedJoi.number().integer() // Bắt buộc phải có
     }),
-    totalWeight: ExtendedJoi.number().when(
+    totalWeight: ExtendedJoi.when(
         'totalWeight', { // Kiểm tra nếu totalWeight không rỗng
         is: ExtendedJoi.exist(),
-        then: ExtendedJoi.required() // Bắt buộc phải có
+        then: ExtendedJoi.number().integer() // Bắt buộc phải có
     })
 });
 
